eb701ba6e8de4abc13e4d541afb05c38988e1b2d
dracaenaa <mbk@kolker.org>
2025-12-05 11:52:24 -0500

    day 2

---
+++ AdventOfCode2025/Day2.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2025;
+
+public class Day2 : IDay
+{
+    public static void RunSolution()
+    {
+        var fileInput = File.ReadAllText("../../../productIds.txt").Split(',');
+
+        long invalidIdsTotal = 0;
+        long currentId;
+        string currentIdString;
+        long lastId;
+
+        for (int i = 0, j = 1; i < fileInput.Length; i++) 
+        {
+            currentIdString = fileInput[i].Split('-')[0];
+            currentId = long.Parse(currentIdString);
+            lastId = long.Parse(fileInput[i].Split("-")[1]);
+
+            while (currentId <= lastId)
+            {
+                if ((currentIdString.Length % 2 == 0) && currentIdString.Substring(0, currentIdString.Length / 2).Contains(currentIdString.Substring(currentIdString.Length / 2)))
+                {
+                    invalidIdsTotal += currentId;
+                }
+
+                currentId++;
+                currentIdString = currentId.ToString();
+            }
+        }
+
+        Console.WriteLine("Total of invalid product IDs: " + invalidIdsTotal);
+    }
+}
