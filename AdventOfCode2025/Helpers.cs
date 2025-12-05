cb967922c168eaa5e3d346d6eab0135861fb7a4e
dracaenaa <mbk@kolker.org>
2025-12-05 11:52:19 -0500

    day 2

--- AdventOfCode2025/Helpers.cs
+++ AdventOfCode2025/Helpers.cs
@@ -11,8 +11,11 @@ public class Helpers
                 case 1:
                     Day1.RunSolution();
                     break;
+                case 2:
+                    Day2.RunSolution();
+                    break;
                 default:
-                    Console.WriteLine("Solution not available, days complete: 1");
+                    Console.WriteLine("Solution not available, days complete: 1 & 2");
                     break;
             }
         } else
