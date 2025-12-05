7fe3cf3fb9568623b3902810265b6a8916eb0b4e
dracaenaa <mbk@kolker.com>
2025-12-05 16:09:40 -0500

    day3 part2

--- AdventOfCode2025/Day3.cs
+++ AdventOfCode2025/Day3.cs
@@ -5,13 +5,18 @@ public class Day3 : IDay
     public static void RunSolution()
     {
         var fileInput = File.ReadAllLines("../../../joltages.txt");
-        //var fileInput = File.ReadAllLines("../../../exampleJoltages.txt");
 
+        Part1(fileInput);
+        Part2(fileInput);
+    }
+
+    public static void Part1(string[] fileInput)
+    {
         int totalOutputJoltage = 0;
         int largestFirstValue = 0;
         int largestSecondValue = 0;
         int index = 0;
-        int currentValue = 0;
+        int currentValue;
 
         foreach (var line in fileInput)
         {
@@ -34,6 +39,48 @@ public class Day3 : IDay
             index = 0;
             largestFirstValue = 0;
             largestSecondValue = 0;
+        }
+
+        Console.WriteLine("Total output joltage: " + totalOutputJoltage);
+    }
+
+    public static void Part2(string[] fileInput)
+    {
+        double totalOutputJoltage = 0;
+        int[] largestValues = [0,0,0,0,0,0,0,0,0,0,0,0];
+        int arrayLength = largestValues.Length;
+        int currentValue;
+        int previousLargestValueIndex = 0;
+
+        for (int i = 0, j = 0, k = arrayLength; i < fileInput.Length;) 
+        {
+            currentValue = int.Parse(fileInput[i].Substring(j, 1));
+            if (currentValue > largestValues[arrayLength - k] && j <= fileInput[i].Length - k)
+            {
+                largestValues[arrayLength - k] = currentValue;
+                previousLargestValueIndex = j;
+            }
+
+            if (j < fileInput[i].Length - k)
+            {
+                j++;
+            }
+            else if (j == fileInput[i].Length - k && k > 1)
+            {
+                k--;
+                j = previousLargestValueIndex + 1;
+            }
+            else if (k == 1 && j == fileInput[i].Length - 1)
+            {
+                var joltage = "";
+                largestValues.Select(value => value.ToString()).ToList().ForEach(value => joltage += value.ToString());
+                totalOutputJoltage += double.Parse(joltage);
+
+                k = arrayLength;
+                i++;
+                j = 0;
+                largestValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            }
         }
 
         Console.WriteLine("Total output joltage: " + totalOutputJoltage);
