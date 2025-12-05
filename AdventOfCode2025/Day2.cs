c5082af05fdca69cda7f07f2e7851e3b57dc98cd
dracaenaa <mbk@kolker.org>
2025-12-05 11:52:30 -0500

    adjustments and start day2 part2

--- AdventOfCode2025/Day2.cs
+++ AdventOfCode2025/Day2.cs
@@ -4,18 +4,25 @@ public class Day2 : IDay
 {
     public static void RunSolution()
     {
-        var fileInput = File.ReadAllText("../../../productIds.txt").Split(',');
+        //var fileInput = File.ReadAllText("../../../productIds.txt").Split(',');
+        var fileInput = File.ReadAllText("../../../exampleProductIds.txt").Split(',');
 
+        Part1(fileInput);
+        Part2(fileInput);
+    }
+
+    private static void Part1(string[] fileInput)
+    {
         long invalidIdsTotal = 0;
         long currentId;
         string currentIdString;
         long lastId;
 
-        for (int i = 0, j = 1; i < fileInput.Length; i++) 
+        foreach (var line in fileInput)
         {
-            currentIdString = fileInput[i].Split('-')[0];
+            currentIdString = line.Split('-')[0];
             currentId = long.Parse(currentIdString);
-            lastId = long.Parse(fileInput[i].Split("-")[1]);
+            lastId = long.Parse(line.Split("-")[1]);
 
             while (currentId <= lastId)
             {
@@ -29,6 +36,34 @@ public class Day2 : IDay
             }
         }
 
-        Console.WriteLine("Total of invalid product IDs: " + invalidIdsTotal);
+        Console.WriteLine("Part 1: Total of invalid product IDs: " + invalidIdsTotal);
+    }
+
+    private static void Part2(string[] fileInput)
+    {
+        long invalidIdsTotal = 0;
+        long currentId;
+        string currentIdString;
+        long lastId;
+
+        foreach (var line in fileInput)
+        {
+            currentIdString = line.Split('-')[0];
+            currentId = long.Parse(currentIdString);
+            lastId = long.Parse(line.Split("-")[1]);
+
+            while (currentId <= lastId)
+            {
+                if (currentIdString.Substring(0, currentIdString.Length / 2).Contains(currentIdString.Substring(currentIdString.Length / 2)))
+                {
+                    invalidIdsTotal += currentId;
+                }
+
+                currentId++;
+                currentIdString = currentId.ToString();
+            }
+        }
+
+        Console.WriteLine("Part 2: Total of invalid product IDs: " + invalidIdsTotal);
     }
 }
