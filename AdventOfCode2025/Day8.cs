b60907060ecce9fbb33ab1c6741868b018d4f957
dracaenaa <mbk@kolker.org>
2025-12-15 11:29:38 -0500

    day 8 in prog

---
+++ AdventOfCode2025/Day8.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2025;
+
+public class Day8 : IDay
+{
+    public static void RunSolution()
+    {
+        //var fileInput = Helpers.ReadFile("../../../fileInputs/junctionBoxes.txt");
+        var fileInput = Helpers.ReadFile("../../../fileInputs/exampleJunctionBoxes.txt");
+
+        Part1(fileInput);
+    }
+
+    private static void Part1(string[] fileInput)
+    {
+        Console.WriteLine("Part 1: ");
+    }
+}
